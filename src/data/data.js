207b13d1de6156cee0e3d3a55f79daf7626fca0a
Rashmika Madaela <rashmikamadaelaofficial@gmail.com>
2026-02-21 20:34:38 +0530

    added some order objects data

---
+++ src/data/data.js
@@ -0,0 +1,20 @@
+const orders = [
+    {
+        id: 1,  
+        description: "Mouse Order from Daraz",
+        pin : "1234",
+
+    },
+    {
+        id: 2,
+        description: "Keyboard Order from Ebay",
+        pin : "5678",
+    },
+    {
+        id: 3,
+        description: "Fried Rice Order from Pickme",
+        pin : "9012",
+    }
+]
+
+export default orders
